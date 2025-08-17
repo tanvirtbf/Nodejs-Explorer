7791a18d580fde5648175b61fe82a3ba63f9ac38
tanvirtbf <tanver018765@gmail.com>
2025-08-17 11:16:59 +0600

    explore require function

--- Fundamentals of Node.js/Module System/app.js
+++ Fundamentals of Node.js/Module System/app.js
@@ -1,5 +1,9 @@
 const sum = require("./sum.js")
-const product = require("./product.js")
+console.log(typeof require);
+// const product = require("./product.js")
 
-console.log(sum(1,2,3,4,5))
-console.log(product(1,2,3,4))
+// console.log(sum(1,2,3,4,5))
+
+console.log("Hello App!");
+// console.log(product(1,2,3,4))
+console.log(sum);
--- Fundamentals of Node.js/Module System/sum.js
+++ Fundamentals of Node.js/Module System/sum.js
@@ -2,4 +2,9 @@ function sum(...nums){
     return nums.reduce((acc, curr) => acc+curr)
 }
 
-module.exports = sum;
+console.log('Hello Sum!');
+for(let i=0; i<5; i++){
+    console.log('i is ', i);
+}
+
+module.exports = "Hello World!"
