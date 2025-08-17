32453549392a281bfac29fb887b5c767b487f862
tanvirtbf <tanver018765@gmail.com>
2025-08-17 11:52:32 +0600

    testing on...

--- Fundamentals of Node.js/Module System/app.js
+++ Fundamentals of Node.js/Module System/app.js
@@ -1,11 +1,9 @@
-// একবার require করলে পুরো sum.js execute হবে
-const sum1 = require("./sum");  
-const sum2 = require("./sum");  
-const sum3 = require("./sum");  
+const sum = require("./sum");
 
-console.log("----- Calling function multiple times -----");
 
-// function বারবার call করা হচ্ছে
-console.log("Result 1:", sum1(2, 3));
-console.log("Result 2:", sum2(5, 7));
-console.log("Result 3:", sum3(10, 20));
+setInterval(() => {
+    console.log(sum.sum2(5, 7));
+    console.log(sum.sum1(5, 7));
+    console.log(sum.sum1(5, 7));
+}, 1000)
+
--- Fundamentals of Node.js/Module System/sum.js
+++ Fundamentals of Node.js/Module System/sum.js
@@ -1,8 +1,18 @@
-console.log("✅ sum.js file executing...");
+console.log('Hello This is Sum File!');
 
-function add(a, b) {
-  console.log(`➡️ Function add(${a}, ${b}) executing...`);
-  return a + b;
+function sum1(...nums){
+    console.log('Sum 1 called');
+    return nums.reduce((acc, curr) => acc + curr)
 }
 
-module.exports = add;
+function sum2(...nums){
+    console.log('Sum 2 called');
+    return nums.reduce((acc, curr) => acc + curr)
+}
+
+function sum3(...nums){
+    console.log('Sum 3 called');
+    return nums.reduce((acc, curr) => acc + curr)
+}
+
+module.exports = {sum1, sum2, sum3};
